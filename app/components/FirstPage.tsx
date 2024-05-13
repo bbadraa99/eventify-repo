e6b1f1775d1502ca1b09ad2c112cd4757ec410d6
Batulzii Badraa <badraa@Badraas-MacBook-Air.local>
2024-05-13 12:46:42 +0800

    First commit

---
+++ app/components/FirstPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FirstPage = () => {
+  return (
+    <div className=''>FirstPage</div>
+  )
+}
+
+export default FirstPage
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,70 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        green: {
+          50: '#30AF5B',
+          90: '#292C27',
+        },
+        purple: {
+          10: '#660099',
+          20: '#3d005b',
+        },
+
+        yellow: {
+          10: '#eae0cc',
+        },
+
+        gray: {
+          10: '#EEEEEE',
+          20: '#A2A2A2',
+          30: '#7B7B7B',
+          50: '#585858',
+          90: '#141414',
+          100: '#F9F9F9',
+        },
+        orange: {
+          50: '#FF814C',
+        },
+        blue: {
+          10: '#023047',
+          20: "#065982",
+          30: '#008ECC',
+          50: '#007AFF',
+          70: '#021639',
+        },
+        background: {
+          10: '#84CEEB',
+          20: '#5680E9',
+          30: '#5AB9EA',
+          40: '#8860D0',
+          50: '#0000FF',
+          60: '#053F5C',
+          70: '#F7AD19',
+          80: '#F27F0C',
+        }
+      },
+      screens: {
+        xs: '400px',
+        '2xl': '1536px',
+        '3xl': '1680px',
+        '4xl': '2200px',
+      },
+      maxWidth: {
+        '10xl': '1512px',
+      },
+      borderRadius: {
+        '5xl': '40px',
+      },
+
+      fontFamily: {
+        code: 'code',
+        dmono: 'dmono',
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
 };
 export default config;
