c3dcaf4ff443b7196bc56f5acdd6e578f30a4138
Yerlen Saifulla <yrlnsayfulla@gmail.com>
2024-06-11 17:18:43 +0900

    changed design of home page

--- app/page.tsx
+++ app/page.tsx
@@ -13,11 +13,11 @@ const Home: React.FC = () => {
 
   return (
     <div className='bg-background-10'>
-      <div className="p-2">
+      <div className="p-0">
         <Header />
-        <div className='h-screen px-28 pb-20 mt-20 flex justify-between '>
-          <div className='text-5xl text-black flex flex-col text-left'>
-            <h1 className='font-medium mt-10 mb-10'>Automate Event <br /> Planning With Friends</h1>
+        <div className={`${styles.hero} flex justify-center text-center`}>
+          <div className='text-5xl text-white flex flex-col'>
+            <h1 className='font-medium mt-40 mb-10'>Automate Event <br /> Planning With Friends</h1>
             <p className='text-xl'>
               Effortlessly plan memorable events with friends using our 
               <br /> automated tools and templates for a stress-free experience.
@@ -26,18 +26,9 @@ const Home: React.FC = () => {
               <button onClick={scrollToMain} className={`${styles.button} ${styles.tryItButton}`}>Try It</button>
             </div>
           </div>
-          <div className='w-2/5 mr-5'>
-            <Image 
-              src="/images/picnic.png" 
-              alt='Picnic image' 
-              className='rounded-5xl '  // Added 'rounded-full' class for rounded image
-              width={100}  // Set the appropriate width here
-              height={100} // Set the appropriate height here
-              layout="responsive" // Ensures the image scales correctly
-            />
-          </div>
         </div>
-        <h2 className="mb-56 text-center text-2xl">Here will be info on how to use our app</h2>
+        <p className='my-56 text-center text-3xl'>Here will be app features</p>
+        <p className='mb-56 text-center text-3xl'>Here will be info on how to use: steps</p>
         <div id="main-section" className={styles.main}>
           <h1 className="text-black text-3xl p-4">Event templates that you can use to create your own</h1>
           <EventGrid />
