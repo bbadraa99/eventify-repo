cdd6c455529c4dbb7151a9f0d0e3e8488f7df8b0
Aibek Minbaev <aibekminbaev050402@gmail.com>
2024-06-11 22:49:41 +0900

    done: creating, fetchig events is working now

---
+++ app/api/eventData/fetchEventData.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import { db } from "../../firebase/config";
+import { doc, getDoc } from "firebase/firestore";
+import { EventData } from "@/app/[createEvent_slug]/page";
+
+// Assuming EventData is correctly defined elsewhere
+export const FetchEventData = (slug: string) => {
+    const [event, setEvent] = useState<EventData>(
+        {
+            title: "",
+            date: new Date(),
+            description: "",
+            template_id: 0,
+            admin: "",
+            guests: [],
+            tasks: [],
+        }
+    );
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const docRef = doc(db, "event_test", slug);
+            const docSnap = await getDoc(docRef);
+
+            if (docSnap.exists()) {
+                setEvent(docSnap.data() as EventData);
+            } else {
+                console.log("No such document!");
+            }
+        };
+
+        fetchData();
+    }, [slug]);
+
+    return event;
+};
--- app/events/[info_slug]/page.tsx
+++ app/events/[info_slug]/page.tsx
@@ -1,16 +1,56 @@
-import React from 'react'
-import Header from '../components/Header'
+'use client';
+
+import React, { useEffect, useReducer, useState } from 'react'
+import Header from '../../components/Header'
 import Image from 'next/image'
-import { EventData } from '../[createEvent_slug]/page'
+import { EventData } from '../../[createEvent_slug]/page'
+import { usePathname } from 'next/navigation';
+import { GetServerSideProps } from 'next';
+import { useRouter } from 'next/router';
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from '@/app/firebase/config';
 
 interface PropElements{
-    eventData: EventData;
+
 }
 
+const Info = () => {
+  const path = usePathname();
+  const eventId = path.split("/")[2];
 
-const Info = (props: PropElements) => {
-  const event = props.eventData;
-  const formattedDate = `${event.date.toUTCString().slice(0, 16)}`;
+  const [event, setEvent] = useState<EventData>(
+      {
+          title: "",
+          date: new Date(),
+          description: "",
+          template_id: 0,
+          admin: "",
+          guests: [],
+          tasks: [],
+      }
+  );
+
+  useEffect(() => {
+      const fetchData = async () => {
+          const docRef = doc(db, "event_test", eventId);
+          console.log("the id: " + eventId);
+          const docSnap = await getDoc(docRef);
+
+          if (docSnap.exists()) {
+              setEvent(docSnap.data() as EventData);
+          } else {
+              console.log("No such document!");
+          }
+      };
+
+      fetchData();
+  }, [eventId]); 
+  
+
+  console.log("Event:" + event.date);
+
+  const formattedDate = new Date(event.date).toUTCString().slice(0, 16);
+  // const path = usePathname();
 
   return (
     <div className='h-full bg-background-10 mx-auto px-36 flex-col'>
@@ -46,7 +86,7 @@ const Info = (props: PropElements) => {
                     <tbody>
                         {event.guests.map((guest, index) => {
                             return (
-                                <tr>
+                                <tr key={index}>
                                 <th>{index+1}</th>
                                 <td>{guest.name}</td>
                                 <td>{guest.email}</td>
@@ -63,3 +103,21 @@ const Info = (props: PropElements) => {
 }
 
 export default Info
+
+// export const getServerSideProps: GetServerSideProps = async (context) => {
+//   const { slug } = context.params!;
+//   const res = await fetchEventData(slug as string);
+//   const data = await res.json();
+
+//   if (res.status !== 200) {
+//     return {
+//       notFound: true,
+//     };
+//   }
+
+//   return {
+//     props: {
+//       event: data,
+//     },
+//   };
+// };
